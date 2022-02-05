ea52eb2ae3dc150ba678fb317a6e5ae5185cc5f2
ryanodonnell2 <rpodonnell2012@gmail.com>
2022-02-04 19:43:55 -0800

    Added Mecanum Drive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,11 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static class DriveTrainConstants{
+        public static final int rightFrontFalconID = 0; 
+        public static final int rightBackFalconID = 1;
+        public static final int leftFrontFalconID = 2; 
+        public static final int leftBackFalconID = 3;
+    }
+}
