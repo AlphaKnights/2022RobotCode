8e5f42b777ff1c411f3d8122f269cb8b96303cd9
Roshan Sood <52641273+YeetOnMyFeet@users.noreply.github.com>
2022-02-09 16:58:31 -0800

    Finished Climbing Code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,7 +33,7 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the button bindings
+    // Configure the button bindings for climbing
     configureButtonBindings();
   }
 
