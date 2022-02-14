2d855d0c6d48cdf1db8125a9b2b8a2286522b12c
sp4c <zackerytafoya@gmail.com>
2022-02-14 15:45:17 -0800

    Added the intake code

---
+++ src/main/java/frc/robot/commands/RunIntake.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.IntakeSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+public class RunIntake extends CommandBase {
+  private final IntakeSubsystem m_sub;
+  private Joystick driveJoystick;
+  public RunIntake(IntakeSubsystem l, Joystick m_driveJoystick) {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    this.driveJoystick = m_driveJoystick;
+    m_sub=l;
+    addRequirements(m_sub);
+    
+  }
+  
+
+  // Called just before this Command runs the first time
+  @Override
+  public void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  public void execute() {
+    m_sub.go(driveJoystick.getRawAxis(OIConstants.joyAxi));
+    
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
