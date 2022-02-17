40814198be15f292580e0583de05269d6e8ddc47
computing-astronomy <87629140+computing-astronomy@users.noreply.github.com>
2022-02-16 21:18:01 -0800

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,17 +1,15 @@
-
-
 package frc.robot.subsystems;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
-  WPI_TalonSRX IntakingMotor;
+  Talon IntakingMotor;
   /**
    * Creates a new IntakeSubsystem.
    */
   public IntakeSubsystem(){
-    IntakingMotor=new WPI_TalonSRX(IntakeConstants.intakeMotorID);
+    IntakingMotor=new Talon(IntakeConstants.intakeMotorID);
   }
 
   public void go(double val){
